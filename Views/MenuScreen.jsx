ff04e4a3a26da2f227e5d53dee2a530a3b2ce334
Alan Jimenez <alanjq@outlook.com>
2023-03-21 20:16:38 -0500

    Archivos base

---
+++ Views/MenuScreen.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+function MenuScreen() {
+    return (
+        <SafeAreaView>
+            <Text>Menú</Text>
+        </SafeAreaView>
+    )
+}
+export default MenuScreen
---
+++ Views/Pantalla2.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+function Pantalla2() {
+    return (
+        <SafeAreaView>
+            <Text>Pantalla 2</Text>
+        </SafeAreaView>
+    )
+}
+export default Pantalla2
